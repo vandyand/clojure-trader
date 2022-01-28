75840e7d0abd812ae7c8f7f1f1e4a1c83808528c
vandyand <vandyand@gmail.com>
2022-01-28 15:47:54 -0500

    added hype_test for hypothesis testing

--- src/hyp_test.clj
+++ src/hyp_test.clj
@@ -9,28 +9,41 @@
   ;;  [clojure.zip :as z]
   ;;  [oz.core :as oz]
   ;;  [clojure.set :as set]
-  ;;  [vec_strategy :as vat]
-  ;;  [strategy :as strat]
+   [vec_strategy :as vat]
+   [strategy :as strat]
    [ga :as ga]
    [stats :as stats]))
 
-;; USE INPUT AND TARGET DATA FROM VAT
-(def best-strat )
+(defn add-delta-return-stream-to-strat [strat]
+  (assoc strat :return-stream-delta (strat/get-stream-delta (strat :return-stream) "return stream delta")))
+
+(def best-strat (add-delta-return-stream-to-strat (first ga/best-strats)))
+
+(def input-and-target-streams-arena (strat/get-input-and-target-streams 4 20))
+
+(def best-strat-arena (add-delta-return-stream-to-strat (vat/get-populated-strat-from-tree (best-strat :tree) input-and-target-streams-arena)))
+
+(ga/plot-strats [best-strat best-strat-arena] input-and-target-streams-arena)
 
 
+(stats/z-score (best-strat :return-stream-delta) (best-strat-arena :return-stream-delta))
+
+(let [arena-return-stream-delta (best-strat-arena :return-stream-delta)]
+ (for [i (range  (count arena-return-stream-delta))] 
+  (let [partial-return-stream-delta (subvec arena-return-stream-delta 0 (+ 1 i))] 
+    (println
+     (stats/z-score (best-strat :return-stream-delta) partial-return-stream-delta)))))
 
 
 ;; TODO
-;; MAKE P-TEST FUNCTION ✅
-;; MAKE SOME EXTENDED INPUT DATA
-;; GET BEST STRATEGY(IES) FROM GA
-;; GET PERFORMANCE ON OUT OF SAMPLE DATA
-;; GET P-TEST VALUE OF PERFORMANCE ON OUT OF SAMPLE DATA
-;; CODIFY THIS AND APPLY AT EVERY TIME STEP OF OOS DATA. STOP STRAT WHEN P-VALUE DIPS TO SOME YET-TO-BE-DETERMINED VALUE
-;; 
-;; START WORKING WITH LIVE DATA
-;;     MAKE INDICATOR GENERATOR FUNCTION?
+;; FINISH INCUBATOR
+;; - USE TRADABLE INSTRUMENT(S) FOR TARGET DATA
+;; - - GET TRADABLE INSTRUMENT HISTORICAL DATA (DAILY IS FINE FOR NOW)
+;; - CREATE INDICATORS MANUALLY (SOME OF THE TRADITIONAL ONES I.E. MA, RSI, BB, ETC... THESE TAKE TRADABLE INSTRUMENTS AS INPUT)
+;; - DESIGN ABSRACT INDICATOR GENERATOR
+;; - BUILD INDICATOR GENERATOR
 ;; 
 ;; NICE TO HAVE
-;;     SPEC EVERY FUNCTION
-;;     FUZZ TEST EVERY FUNCTION WITH SPEC GENERATORS
+;; - SPEC EVERY FUNCTION
+;; - FUZZ TEST EVERY FUNCTION WITH SPEC GENERATORS
+;;
