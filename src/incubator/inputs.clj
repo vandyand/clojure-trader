74f37c853d27398c81858535d308cb3409f7c0a4
vandyand <vandyand@gmail.com>
2022-02-16 17:34:10 -0500

    refactor: inputs from strategy

---
+++ src/incubator/inputs.clj
@@ -0,0 +1,39 @@
+(ns incubator.inputs
+  (:require [incubator.sine_waves :as sw]))
+
+(defn scaled-rand-dbl
+  "returns random double between min (inclusive) and max (exclusive)"
+  [min max]
+  (-> (- max min) (rand) (+ min)))
+
+(defn get-random-sine-config [input-config]
+  (let [rand-amp (scaled-rand-dbl 0 (input-config :max-amp))
+        rand-freq (scaled-rand-dbl 0 (input-config :max-freq))
+        rand-h-shift (scaled-rand-dbl 0 (input-config :max-h-shift))
+        rand-v-shift (scaled-rand-dbl 0 (input-config :max-v-shift))
+        args {:amp rand-amp :freq rand-freq :h-shift rand-h-shift :v-shift rand-v-shift}
+        name (sw/get-sine-fn-name args)]
+    {:name name
+     :args args
+     :fn (fn [x]
+           (-> x
+               (* rand-freq)
+               (- rand-h-shift)
+               (Math/sin)
+               (* rand-amp)
+               (+ rand-v-shift)))}))
+
+(defn get-sine-configs [base-config num-key]
+  (vec (repeatedly (get base-config num-key) #(get-random-sine-config base-config))))
+
+(defn get-input-config [num-inception-streams num-intention-streams num-data-points max-amp max-freq max-v-shift max-h-shift]
+  (let [base-config {:num-inception-streams num-inception-streams
+                     :num-intention-streams num-intention-streams
+                     :num-data-points num-data-points
+                     :max-amp max-amp
+                     :max-freq max-freq
+                     :max-v-shift max-v-shift
+                     :max-h-shift max-h-shift}]
+    (assoc base-config
+           :inception-streams-config (get-sine-configs base-config :num-inception-streams)
+           :intention-streams-config (get-sine-configs base-config :num-intention-streams))))
--- src/incubator/strategy.clj
+++ src/incubator/strategy.clj
@@ -9,7 +9,8 @@
    [clojure.zip :as z]
    [oz.core :as oz]
    [clojure.set :as set]
-   [incubator.sine_waves :as sw]))
+   [incubator.sine_waves :as sw]
+   [incubator.inputs :as inputs]))
 
 ;; START SERVER FOR VISUALIZATION
 
@@ -18,43 +19,6 @@
 ;; CONFIG FUNCTIONS
 
 ;; You apply a strategy to source and target inputs to produce a return stream
-
-(defn scaled-rand-dbl
-  "returns random double between min (inclusive) and max (exclusive)"
-  [min max]
-  (-> (- max min) (rand) (+ min)))
-
-(defn get-random-sine-config [input-config]
-  (let [rand-amp (scaled-rand-dbl 0 (input-config :max-amp))
-        rand-freq (scaled-rand-dbl 0 (input-config :max-freq))
-        rand-h-shift (scaled-rand-dbl 0 (input-config :max-h-shift))
-        rand-v-shift (scaled-rand-dbl 0 (input-config :max-v-shift))
-        args {:amp rand-amp :freq rand-freq :h-shift rand-h-shift :v-shift rand-v-shift}
-        name (sw/get-sine-fn-name args)]
-    {:name name
-     :args args
-     :fn (fn [x]
-           (-> x
-               (* rand-freq)
-               (- rand-h-shift)
-               (Math/sin)
-               (* rand-amp)
-               (+ rand-v-shift)))}))
-
-(defn get-sine-configs [base-config num-key]
-  (vec (repeatedly (get base-config num-key) #(get-random-sine-config base-config))))
-
-(defn get-input-config [num-inception-streams num-intention-streams num-data-points max-amp max-freq max-v-shift max-h-shift]
-  (let [base-config {:num-inception-streams num-inception-streams
-                     :num-intention-streams num-intention-streams
-                     :num-data-points num-data-points
-                     :max-amp max-amp
-                     :max-freq max-freq
-                     :max-v-shift max-v-shift
-                     :max-h-shift max-h-shift}]
-    (assoc base-config
-           :inception-streams-config (get-sine-configs base-config :num-inception-streams)
-           :intention-streams-config (get-sine-configs base-config :num-intention-streams))))
 
 (defn get-tree-config [min-depth max-depth index-pairs]
   {:min-depth min-depth :max-depth max-depth :index-pairs index-pairs})
@@ -254,7 +218,7 @@
    (get-plot-values
     (map :return-stream strats))))
 
-(def input-config (get-input-config 4 2 1000 10 0.1 0 100))
+(def input-config (inputs/get-input-config 4 2 1000 10 0.1 0 100))
 (def tree-config (get-tree-config 2 6 (get-index-pairs (get input-config :num-inception-streams))))
 (def strat1 (get-populated-strat input-config tree-config))
 (def strat2 (get-populated-strat input-config tree-config))
