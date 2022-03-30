9cf84c7c08a0436eeb82e38734066978a5d6ce2e
vandyand <vandyand@gmail.com>
2022-03-30 11:45:25 -0400

    alien computer test

--- src/v0_2_X/factory.clj
+++ src/v0_2_X/factory.clj
@@ -6,12 +6,12 @@
    [file :as file]))
  
  (def backtest-config (config/get-backtest-config-util
-                      ["EUR_USD" "both" "AUD_USD" "inception" "GBP_USD" "inception" "USD_JPY" "inception" 
-                       "EUR_GBP" "inception" "EUR_JPY" "inception" "USD_CHF" "inception" "AUD_JPY" "inception" 
-                       "USD_CAD" "inception" "AUD_CAD" "inception" "CHF_JPY" "inception" "EUR_CHF" "inception" 
-                       "NZD_USD" "inception" "EUR_CAD" "inception" "NZD_JPY" "inception" "AUD_CHF" "inception" 
-                       "CAD_CHF" "inception" "GBP_CHF" "inception" "EUR_AUD" "inception" "GBP_CAD" "inception"]
-                      ;; ["EUR_USD" "both"]
+                      ;; ["EUR_USD" "both" "AUD_USD" "inception" "GBP_USD" "inception" "USD_JPY" "inception" 
+                      ;;  "EUR_GBP" "inception" "EUR_JPY" "inception" "USD_CHF" "inception" "AUD_JPY" "inception" 
+                      ;;  "USD_CAD" "inception" "AUD_CAD" "inception" "CHF_JPY" "inception" "EUR_CHF" "inception" 
+                      ;;  "NZD_USD" "inception" "EUR_CAD" "inception" "NZD_JPY" "inception" "AUD_CHF" "inception" 
+                      ;;  "CAD_CHF" "inception" "GBP_CHF" "inception" "EUR_AUD" "inception" "GBP_CAD" "inception"]
+                      ["EUR_USD" "both"]
                       "binary" 3 6 10 1200 "H1"))
 
 (def ga-config (config/get-ga-config 20 backtest-config (config/get-pop-config 20 0.4 0.3 0.5)))
