7c232cf22de16883e3086757950e98a9d054b7d6
vandyand <vandyand@gmail.com>
2025-03-04 21:48:49 -0500

    tiny change - switched to demo

--- src/portfolio.clj
+++ src/portfolio.clj
@@ -129,9 +129,9 @@
                                      :back-pct 0.77}}]
     (arena/run-and-save-backtest backtest-params)))
 
-#_(shoot-money-x-from-backtest-y 100 (run-backtest constants/pairs-by-liquidity-oanda))
+(shoot-money-x-from-backtest-y 100 (run-backtest constants/pairs-by-liquidity-oanda))
 #_(shoot-money-x-from-backtest-y 100 (run-backtest constants/pairs-by-liquidity-crypto))
-(shoot-money-x-from-backtest-y 120 (run-backtest constants/pairs-by-liquidity))
+#_(shoot-money-x-from-backtest-y 120 (run-backtest constants/pairs-by-liquidity))
 
 (defn get-accounts-worth []
   (let [oanda-nav (oanda_api/get-account-nav)
